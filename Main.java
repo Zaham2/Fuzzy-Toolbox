37b729814569412c9a3a911972b64526d8e96494
Zaham2 <zaham2@yahoo.com>
2019-11-10 10:54:26 +0200

    Completed rules. Implementing Defuzzification

--- Main.java
+++ Main.java
@@ -3,6 +3,18 @@ import java.util.Vector;
 
 public class Main {
 
+    static class Vertex {
+
+        double x;
+        double y;
+
+        public Vertex(double a, double b) {
+            x = a;
+            y = b;
+        }
+
+    }
+
     static class FuzzyVar {
 
         String name;
@@ -53,10 +65,20 @@ public class Main {
 
         String name;
         String type;  //0 is triangle... 1 is trapezium
+        double fuzzyMembership;
+
+        public double getFuzzyMembership() {
+            return fuzzyMembership;
+        }
+
+        public void setFuzzyMembership(double fuzzyMembership) {
+            this.fuzzyMembership = fuzzyMembership;
+        }
+
         int nVertices;
 
 
-        Vector<Double> vertices;
+        Vector<Vertex> vertices;
 
         public void setName(String setName) {
             this.name = setName;
@@ -67,7 +89,7 @@ public class Main {
         }
 
         public void setVerticesVector() {
-            vertices = new Vector<Double>();
+            vertices = new Vector<Vertex>();
         }
 
         public void setnVertices() {
@@ -82,7 +104,7 @@ public class Main {
             return nVertices;
         }
 
-        public Vector<Double> getVertices() {
+        public Vector<Vertex> getVertices() {
             return vertices;
         }
 
@@ -90,8 +112,17 @@ public class Main {
             return name;
         }
 
+        public void setYs() {
 
+            if (this.type.equals("triangle")) {
+                vertices.get(1).y = 1;
+            } else {
+                vertices.get(1).y = 1;
+                vertices.get(2).y = 1;
+            }
+        }
     }
+
 
     public static void main(String[] args) {
 
@@ -120,17 +151,203 @@ public class Main {
                 lines = sc.nextLine();
                 strs = lines.trim().split("\\s+");
                 for (String s : strs)
-                    set.getVertices().add(Double.parseDouble(s));
+                    set.getVertices().add(new Vertex(Double.parseDouble(s), 0));
 
 
+                set.setYs();
                 var.getSets().add(set);
             }
             vars.add(var);
         }
 
-//        printVar(vars.get(0));
-        fuzzify(vars);
+        fuzzify(vars);  //for each Fuzzy set in each fuzzy var... calculate the set's membership
 
+
+        //---------------------
+        // Output var
+        //---------------------
+
+        FuzzyVar output = new FuzzyVar();
+        output.setName(sc.nextLine());
+        output.setSetNum(Integer.parseInt(sc.nextLine()));
+        output.setSetsVector();
+
+        for (int j = 0; j < output.getSetNum(); j++) {
+            FuzzySet set = new FuzzySet();
+            String lines = sc.nextLine();
+            String[] strs = lines.trim().split("\\s+");
+            set.setName(strs[0]);
+            set.setType(strs[1]);
+            set.setVerticesVector();
+            set.setnVertices();
+
+            lines = sc.nextLine();
+            strs = lines.trim().split("\\s+");
+            for (String s : strs)
+                set.getVertices().add(new Vertex(Double.parseDouble(s), 0));
+
+
+            set.setYs();
+            output.getSets().add(set);
+        }
+
+        printVar(vars.get(0));
+        printVar(vars.get(1));
+        printVar(vars.get(2));
+        printVar(output);
+
+
+        //----------------------
+        //Rules
+        //-----------------------
+
+        int nRules = Integer.parseInt(sc.nextLine());
+        String[] rules = new String[nRules];
+        for (int i = 0; i < nRules; i++)
+            rules[i] = sc.nextLine();
+
+        for (String s : rules)
+            System.out.println(s);
+
+        Vector<Double> fuzzyRulesResults = new Vector<>();
+        String fuzzyOutputSet = "";
+
+        for (int i = 0; i < nRules; i++) {
+
+            String[] strs = rules[i].split("\\s=\\s|\\s");
+            fuzzyOutputSet = strs[strs.length - 1];
+
+            int nConditions = Integer.parseInt(strs[0]);
+            double resultForThisRule = 0;
+            Vector<Double> conditionValues = new Vector<>(nConditions);
+            Vector<String> conditionOperators = new Vector<>(nConditions - 1);
+
+
+            for (int j = 0; ; ) {
+                String fuzzyVarToLookFor = strs[++j];
+                String fuzzySetToLookFor = strs[++j];
+
+                //This part calculates the condition's membership value
+                conditionValues.add(lookForSetMembership(vars, fuzzyVarToLookFor, fuzzySetToLookFor));
+                //We have calculated the condition's membership and added it to the conditionValues vector
+
+                ++j;
+                if (strs[j].equals("AND") || strs[j].equals("OR")) {
+                    conditionOperators.add(strs[j]);
+                } else if (strs[j].equals("then")) {
+                    break;
+                }
+            }
+
+            int c = 0;
+            for (int k = 0; k < conditionOperators.size(); k++) {
+
+                if (conditionOperators.get(k).equals("AND"))
+                    resultForThisRule = AND(conditionValues.get(c), conditionValues.get(++c));
+                else
+                    resultForThisRule = OR(conditionValues.get(c), conditionValues.get(++c));
+            }
+            fuzzyRulesResults.add(resultForThisRule);
+        }
+
+        System.out.println("Memberships");
+        for (FuzzyVar var : vars) {
+            for (FuzzySet set : var.sets) {
+                System.out.println(set.name + " = " + set.fuzzyMembership);
+            }
+        }
+
+        System.out.println();
+        System.out.println("Results of fuzzy rules");
+        for (double d : fuzzyRulesResults) {
+            System.out.println(d);
+        }
+        System.out.println(fuzzyOutputSet);
+        //------------------------
+        //Defuzzification
+        //------------------------
+
+
+        //What am I trying to do??
+        //I want to use the results from the statements before " then "
+        //1- get results from fuzzyOutputSet (one entry for each fuzzy rule... so anding and oring already complete)
+        //2- know which variable to set in (FuzzyVar)output
+        //3- make that set equal to the corresponding fuzzyOutputSet
+        //4- for each FuzzySet in output:
+        //  4.1- calculate its centroidArea
+        //  4.2- calculate its xCentroid
+        //  4.3- multiply the xCentroid by the fuzzyMembership
+        //  4.4 calculate the weighted mean using what you calculated in 4.3 --> This is the defuzzified value
+
+    }
+
+    static class FuzzyOutputPair{
+
+        //This this helps keep track of the output FuzzyVar's sets when we assign membership
+
+        String name;
+        double membership;
+    }
+
+    private static double calculateCentroidArea(FuzzySet set) {
+
+        double area = 0;
+
+        for (int i = 0; i < set.nVertices - 1; i++) {
+            double xi = set.vertices.get(i).x;
+            double yi = set.vertices.get(i).y;
+            double xiplus1 = set.vertices.get(i + 1).x;
+            double yiplus1 = set.vertices.get(i + 1).y;
+
+            area += (xi * yiplus1) - (xiplus1 * yi);
+        }
+
+        return (0.5 * area);
+    }
+
+    private static double calculatexCentroid(FuzzySet set, double area) {
+
+        double xCentroidVal = 0;
+
+        for (int i = 0; i < set.nVertices - 1; i++) {
+            double xi = set.vertices.get(i).x;
+            double yi = set.vertices.get(i).y;
+            double xiplus1 = set.vertices.get(i + 1).x;
+            double yiplus1 = set.vertices.get(i + 1).y;
+
+            xCentroidVal += (xi + xiplus1) * ((xi * yiplus1) - (xiplus1 * yi));
+        }
+
+        return (1 / (area * 6)) * xCentroidVal;
+    }
+
+    public static double AND(double d1, double d2) {
+        return Math.min(d1, d2);
+    }
+
+    public static double OR(double d1, double d2) {
+        return Math.max(d1, d2);
+    }
+
+    public static double lookForSetMembership(Vector<FuzzyVar> vars, String fuzzyVarToLookFor, String fuzzySetToLookFor) {
+
+        for (FuzzyVar var : vars) {
+            if (var.name.equals(fuzzyVarToLookFor)) {
+                for (FuzzySet set : var.sets) {
+                    if (set.name.equals(fuzzySetToLookFor)) {
+                        return set.fuzzyMembership;
+
+                    }
+                }
+
+            }
+
+        }
+
+        System.out.println("Could not find the fuzzySet " + fuzzySetToLookFor + " in fuzzyVar " + fuzzyVarToLookFor);
+        System.out.println("Terminating. \nPlease insert correct values next time");
+        System.exit(0);
+        return 0;
     }
 
     private static void fuzzify(Vector<FuzzyVar> vars) {
@@ -138,23 +355,73 @@ public class Main {
         for (FuzzyVar var : vars) {
 
             double crisp = var.crispValue;
-            //now I want to find the sets that this crisp value belongs to
-
+            Vertex vertex1 = null, vertex2 = null;
             for (FuzzySet set : var.sets) {
 
-                for (int k = 0; k < set.nVertices; k++) {
-
-                    if (crisp < set.vertices.get(k)){
-                        //get this point and the point before it
-                        //Postpone this part till we implement the
-                        // a way to keep track of points
-                    }
-
+                set.fuzzyMembership = 0;
+                if (doesIntersect(set, crisp)) {
+                    Vertex[] tempv = getIntersectionLinePoints(set, crisp);
+                    vertex1 = tempv[0];
+                    vertex2 = tempv[1];
+                    set.fuzzyMembership = calculateMembership(vertex1, vertex2, crisp);
                 }
+
             }
 
 
         }
+    }
+
+    private static double calculateMembership(Vertex vertex1, Vertex vertex2, double crisp) {
+
+        double gradient;
+        double yIntercept;
+
+        double x1 = vertex1.x;
+        double x2 = vertex2.x;
+        double y1 = vertex1.y;
+        double y2 = vertex2.y;
+
+        if (x2 - x1 == 0) {
+            System.out.println("Gradient is undefined");
+            System.out.println("Assuming membership = 0");
+            return 0;
+        }
+
+        gradient = ((y2 - y1) / (x2 - x1));
+        yIntercept = y1 - (x1 * gradient);
+
+        double membership = (gradient * crisp) + yIntercept;
+        return membership;
+    }
+
+    public static boolean doesIntersect(FuzzySet set, double crisp) {
+
+
+        for (int i = 0; i < set.nVertices - 1; i++) {
+
+            if (crisp >= set.vertices.get(i).x && crisp <= set.vertices.get(i + 1).x) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public static Vertex[] getIntersectionLinePoints(FuzzySet set, double crisp) {
+
+        Vertex[] verts = new Vertex[2];
+        for (int k = 0; k < set.nVertices - 1; k++) {
+
+            if (crisp >= set.vertices.get(k).x && crisp <= set.vertices.get(k + 1).x) {
+                verts[0] = set.vertices.get(k);
+                verts[1] = set.vertices.get(k + 1);
+                break;
+
+            }
+        }
+
+        return verts;
     }
 
     public static void printVar(FuzzyVar var) {
@@ -164,8 +431,8 @@ public class Main {
         for (int i = 0; i < var.setNum; i++) {
 
             System.out.println(var.sets.get(i).name + " " + var.sets.get(i).type);
-            for (Double j : var.sets.get(i).getVertices())
-                System.out.print(j + "  ");
+            for (Vertex j : var.sets.get(i).getVertices())
+                System.out.print("(" + j.x + " , " + j.y + ")");
             System.out.println();
 
         }
